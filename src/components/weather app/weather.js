c711bdd79826b0b62ea1665da5700890ef176170
Dilpreet0501 <100590445+Dilpreet0501@users.noreply.github.com>
2023-05-01 14:43:13 +0530

    edit change

--- src/components/weather app/weather.js
+++ src/components/weather app/weather.js
@@ -4,36 +4,42 @@ import Weathercard from "./weathercard"
 const Weather = () => {
   const[searchValue,setSearchValue]=useState("new delhi ")
   const[tempinfo,setTempInfo]=useState(" ")
-  const getWeatherInfo=async()=>
-  { try {
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=dc91f8c5741a2d36c6a9fd28efb5de68`;
-    let res=await fetch(url);
-    let data= await res.json();
-    const {temp, humidity,pressure}=data.main;
-    const{main: weathermood}=data.weather[0];
-    const{name}=data;
-    const{speed}=data.wind;
-    const{country,sunset}=data.sys;
-
-    const myNewWeatherInfo={
-      temp,
-      humidity,pressure,
-      weathermood,
-      name,
-      speed,
-      country,
-      sunset
-    };
+  const [dataVal, setDataVal]= useState(" ")
+  useEffect(()=>{ const getWeatherInfo=async()=>
+    { try {
+      let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=dc91f8c5741a2d36c6a9fd28efb5de68`;
+      let res=await fetch(url);
+      let data= await res.json();
+      const {temp, humidity,pressure}=data.main;
+      const{main: weathermood}=data.weather[0];
+      const{name}=data;
+      const{speed}=data.wind;
+      const{country,sunset}=data.sys;
+  
+      const myNewWeatherInfo={
+        temp,
+        humidity,pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset
+      };
+      
+      setTempInfo(myNewWeatherInfo);
+      
+    } catch (error) {
+      console.log(error)
+    }
+  
+    }
     
-    setTempInfo(myNewWeatherInfo);
     
-  } catch (error) {
-    console.log(error)
+    getWeatherInfo();
+  },[searchValue]);
+  const change=()=>{
+    setSearchValue(dataVal);
   }
-
-  }
-  useEffect(()=>{getWeatherInfo();
-  },[]);
   return (
     <>
     <div className='wrap'> 
@@ -44,8 +50,8 @@ const Weather = () => {
       id="search"
       className='searchTerm'
      //value={searchValue}
-     onChange={(e)=>setSearchValue(e.target.value)}></input>
-<button className="searchButton" type="button" onClick={getWeatherInfo}>
+     onChange={(e)=>setDataVal(e.target.value)}></input>
+<button className="searchButton" type="button" onClick={change}>
 Search
 </button>
     </div> 
